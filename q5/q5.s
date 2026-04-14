d6036e654080b2d5e0fc1e85642c54e4ef46479c
Arush Satarkar <130154022+TonyStark701@users.noreply.github.com>
2026-04-14 18:09:55 +0530

    q5 update

--- q5/q5.s
+++ q5/q5.s
@@ -15,33 +15,33 @@ main:
     sd ra, 8(sp)
     sd s0, 0(sp)
 
-    la a0, filename
-    la a1, mode
-    call fopen
+    la a0, filename #loading the address of the string as the first arguement
+    la a1, mode     #loading the "r" mode
+    call fopen      #calling the func with above two args
 
-    # now suppose I have the pointer in a0
+    # now I have the pointer in a0
 
-    mv s0, a0
-    li a1, 0
-    li a2, 2
+    mv s0, a0   #moving it for other function calls
+    li a1, 0    #offset from the start
+    li a2, 2    #end of the file
     call fseek
-    mv a0,s0
-    call ftell
+    mv a0,s0    #moving the pointer back
+    call ftell  #to check the size of the file(cursor and start difference)
 
-    li s1, 0
+    li s1, 0    #for the before offset
     mv s2, a0   # s2 contains the number of characters now
     addi s2, s2, -1     #stores the left offset
 
 recur:
-    bge s1, s2, success
-    mv a0,s0
-    mv a1, s1
+    bge s1, s2, success     #if the pointers cross, then success
+    mv a0,s0    #file pointer
+    mv a1, s1   #offset
     li a2, 0
-    call fseek
+    call fseek  #the cursor gets updated and goes there
     mv a0, s0
     call fgetc
     #now a0 contains the first element
-    mv t0, a0
+    mv t0, a0   #moving it to t0
 
     mv a0,s0
     mv a1, s2
@@ -50,10 +50,10 @@ recur:
     mv a0, s0
     call fgetc
     #now a0 contains the second element
-    mv t1, a0
+    mv t1, a0   #moving it to t1 for comparison with t0
     bne t0,t1, fail
     addi s1, s1, 1
-    addi s2, s2, -1
+    addi s2, s2, -1     #updating the offsets
 
     j recur
     
@@ -61,7 +61,7 @@ recur:
 
 fail:
     #printf no
-    la a0, nope
+    la a0, nope #loading the address of the no string for printing
     call printf
     ld s2, 24(sp)
     ld s1, 16(sp)
@@ -73,7 +73,7 @@ fail:
 success:
     #printf
     la a0, yes
-    call printf
+    call printf     #printing yes
     ld s2, 24(sp)
     ld s1, 16(sp)
     ld ra, 8(sp)
